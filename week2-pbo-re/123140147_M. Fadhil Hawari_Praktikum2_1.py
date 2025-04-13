5d2bbd1c7b2d60a62bc0b44d0b592e6b68f5223c
hawaricracker <63854180+hawaricracker@users.noreply.github.com>
2025-04-13 15:14:05 +0700

    Update 123140147_M. Fadhil Hawari_Praktikum2_1.py

--- week2-pbo-re/123140147_M. Fadhil Hawari_Praktikum2_1.py
+++ week2-pbo-re/123140147_M. Fadhil Hawari_Praktikum2_1.py
@@ -1,23 +1,40 @@
 import math
 
-angka = input("Masukkan Angka: ")
+class calculator():
+    def __init__(self, a):
+        self.a = a
+    
+    def __add__(self, other):
+        return calculator(self.a + other.a)
+    
+    def __mul__(self, other):
+        return calculator(self.a * other.a)
+    
+    def __truediv__(self, other):
+        if other.a == 0:
+            raise ZeroDivisionError("Division by zero is not allowed.")
+        return calculator(self.a / other.a)
+    
+    def __sub__(self, other):
+        return calculator(self.a - other.a)
+    
+    def __log__(self, other):
+        if other.a <= 0:
+            raise ValueError("Logarithm base must be greater than zero.")
+        return calculator(math.log(self.a))
+    
+    def __pow__(self, other):
+        return calculator(self.a ** other.a)
+    
+    def __str__(self):
+        return str(self.a)
+    
+calc1 = calculator(10)
+calc2 = calculator(5)
 
-try:
-    try:
-        angka = int(angka)
-    except ValueError:
-        print("Input tidak valid. Harap masukkan angka yang valid.")
-        raise TypeError
-
-    if angka < 0:
-        raise ValueError("Angka tidak boleh negatif.")
-    elif angka == 0:
-        raise ValueError("Angka tidak boleh nol.")
-    result = math.sqrt(angka)
-    print(result)
-
-except ValueError as e:
-    print(e)
-
-except TypeError:
-    pass
+print(calc1 + calc2)  # Output: 15
+print(calc1 - calc2)  # Output: 5
+print(calc1 * calc2)  # Output: 50
+print(calc1 / calc2)  # Output: 2.0
+print(calc1 ** calc2)  # Output: 100000
+print(calc1.__log__(calc2))  # Output: 2.3
